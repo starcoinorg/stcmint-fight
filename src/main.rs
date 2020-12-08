042cab712e1d4c4c70d0a7488425038f04471151
0xf86 <458041958@qq.com>
2020-12-08 11:29:33 +0800

    Update main vars

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,14 @@ use stcmint_fight::Race;
 
 fn main() {
     let path = std::path::Path::new("/Users/fikgol/workspaces/stcmint-fight/starcoindb");
-    let chain = BlockSnapshot::load_from_db(path, 1607090400, 1607392800).unwrap();
-    let luckies = Race::select(2, &chain);
-    let topies = Race::top(2, &chain);
+    let start_timestamp = 1607090400;
+    let end_timestamp = 1607392800;
+    let top_n = 10;
+    let lucky_n = 2;
+
+    let chain = BlockSnapshot::load_from_db(path, start_timestamp, end_timestamp).unwrap();
+    let luckies = Race::select(top_n, &chain);
+    let topies = Race::top(lucky_n, &chain);
     println!("Top winners: {:?}", topies);
     println!("Lucky winners: {:?}", luckies);
 }
